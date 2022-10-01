098fe04436e86da2bce0e978b4a98cccad8d4530
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2022-10-01 11:28:26 +0300

    Ioannis E. Kommas

--- HR Data Analysis/task/explore.py
+++ HR Data Analysis/task/explore.py
@@ -34,3 +34,29 @@ if __name__ == '__main__':
         # All data in now loaded to the Data folder.
 
     # write your code here
+
+    # XML to DF
+    df_A_office_data = pd.read_xml('../Data/A_office_data.xml')
+    df_B_office_data = pd.read_xml('../Data/B_office_data.xml')
+    df_hr_data = pd.read_xml('../Data/hr_data.xml')
+
+    index_a = df_A_office_data.employee_office_id.values
+    index_a = ["A"+ str(i) for i in index_a]
+
+    index_b = df_B_office_data.employee_office_id.values
+    index_b = ["B"+ str(i) for i in index_b]
+
+    df_A_office_data['index'] = index_a
+    df_A_office_data.set_index('index', drop=True, inplace=True)
+
+    df_B_office_data['index'] = index_b
+    df_B_office_data.set_index('index', drop=True, inplace=True)
+
+    df_hr_data.set_index("employee_id", drop=True, inplace=True)
+
+    print(df_A_office_data.index.to_list())
+    print(df_B_office_data.index.to_list())
+    print(df_hr_data.index.to_list())
+
+
+
